70bc5e7b360547cd2e1bfaf4410dd52b80cc68e7
Alhamsya Bintang Dyasta <alhamsya@gmail.com>
2021-05-14 04:42:42 +0700

    update unit testing

--- different_test.go
+++ different_test.go
@@ -1,8 +1,12 @@
 package different
 
 import (
+	"errors"
 	"reflect"
 	"testing"
+
+	"github.com/r3labs/diff/v2"
+	"github.com/undefinedlabs/go-mpatch"
 )
 
 func TestGenerateDiff(t *testing.T) {
@@ -22,6 +26,7 @@ func TestGenerateDiff(t *testing.T) {
 	tests := []struct {
 		name    string
 		args    args
+		patch   func()
 		want    []byte
 		wantErr bool
 	}{
@@ -37,6 +42,7 @@ func TestGenerateDiff(t *testing.T) {
 					Age:  12,
 				},
 			},
+			patch:   func() {},
 			want:    []byte(`[{"Name":{"before":"Alhamsya","after":"Bintang"}}]`),
 			wantErr: false,
 		},
@@ -46,17 +52,44 @@ func TestGenerateDiff(t *testing.T) {
 				origin: nil,
 				new:    nil,
 			},
+			patch:   func() {},
 			want:    nil,
 			wantErr: false,
 		},
 		{
-			name: "When_deepEqual_ReturnNil",
+			name: "When_diff_ReturnError",
 			args: args{
-				origin: nil,
-				new:    nil,
+				origin: &User{
+					Name: "Alhamsya",
+					Age:  10,
+				},
+				new: &User{
+					Name: "Bintang",
+					Age:  12,
+				},
+			},
+			patch: func() {
+				var guard *mpatch.Patch
+				guard, _ = mpatch.PatchMethod(diff.Diff, func(a, b interface{}, opts ...func(d *diff.Differ) error) (diff.Changelog, error) {
+					defer guard.Unpatch()
+					return nil, errors.New("error diff.Diff")
+				})
 			},
 			want:    nil,
-			wantErr: false,
+			wantErr: true,
+		},
+		{
+			name: "When_typeDiffNotUpdate_ReturnError",
+			args: args{
+				origin: &User{
+					Name: "Alhamsya",
+					Age:  10,
+				},
+				new: nil,
+			},
+			patch:   func() {},
+			want:    nil,
+			wantErr: true,
 		},
 		{
 			name: "When_originParamIsNil_ReturnError",
@@ -67,24 +100,14 @@ func TestGenerateDiff(t *testing.T) {
 				},
 				new: &Dummy{},
 			},
+			patch:   func() {},
 			want:    nil,
 			wantErr: true,
-		},
-		{
-			name: "When_originParamIsNil_ReturnError",
-			args: args{
-				origin: &User{},
-				new: &User{
-					Name: "Alhamsya",
-					Age:  10,
-				},
-			},
-			want:    []byte(`[{"Name":{"before":"","after":"Alhamsya"}}]`),
-			wantErr: false,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			tt.patch()
 			got, err := GenerateDiff(tt.args.origin, tt.args.new)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("GenerateDiff() error = %v, wantErr %v", err, tt.wantErr)
--- go.mod
+++ go.mod
@@ -11,6 +11,7 @@ require (
 	github.com/pkg/errors v0.9.1
 	github.com/r3labs/diff/v2 v2.13.0
 	github.com/stretchr/testify v1.6.1 // indirect
+	github.com/undefinedlabs/go-mpatch v1.0.6
 	golang.org/x/net v0.0.0-20210510120150-4163338589ed // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
